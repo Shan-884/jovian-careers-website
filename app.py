e19a7de555cb7632c6a36c71cf1611d980a112bd
Shan-884 <zeeshan.saeed1790@gmail.com>
2024-12-03 17:29:45 +0000

    Changes in html and css

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask    #from minimal progrAM FROM QUICK Start flask
+from flask import Flask, render_template   #from minimal progrAM FROM QUICK Start flask
 
 app = Flask(__name__)   #Flask application
 
 @app.route("/")   #it is basically the path or name after domain name i.e google.com/pics in this pics is the route.
-def hello_world():
-    return "Hello, World!"
+def hello_jovian():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0', debug=True)
